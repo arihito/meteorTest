23c84aa289bbfcf5442dcd7396259436b4794a2c
松田 有人 <arihito@matsuda-no-MacBook-Pro.local>
2015-10-04 14:39:50 +0900

    チェックボックスと削除ボタンの機能追加

--- sample-app.js
+++ sample-app.js
@@ -6,12 +6,12 @@ if (Meteor.isClient) {
     tasks: function () {
       //mongoDBの中の情報をTasks変数内に取得
       //createAt:-1で新しい順
-      return Tasks.find({},{sort: {createAt: -1}});
+      return Tasks.find({},{sort: {createdAt: -1}});
     }
   });
 
+  //クラス指定したformに対して関数の定義
   Template.body.events({
-    //クラス指定したformに対して関数の定義
     "submit .new-task": function (event) {
       event.preventDefault();
       //textフォームの入力値の取得
@@ -22,6 +22,18 @@ if (Meteor.isClient) {
         createdAt: new Date() // current time
       });
       event.target.text.value = "";
+    }
+  })
+
+  // チェックボックスと削除ボタンの機能追加
+  Template.task.events({
+    "click .toggle-checked": function() {
+      Tasks.update(this._id, {
+        $set: {checked: ! this.checked}
+      });
+    },
+    "click .delete": function() {
+      Tasks.remove(this._id);
     }
   })
 }
