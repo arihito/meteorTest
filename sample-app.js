7bc62265a9a9a98672045e22e827c0b5c8a84b71
松田 有人 <arihito@matsuda-no-MacBook-Pro.local>
2015-10-04 14:24:44 +0900

    フォームの入力値をDBに登録してリストに表示

--- sample-app.js
+++ sample-app.js
@@ -7,4 +7,19 @@ if (Meteor.isClient) {
       return Tasks.find({});
     }
   });
+
+  Template.body.events({
+    //クラス指定したformに対して関数の定義
+    "submit .new-task": function (event) {
+      event.preventDefault();
+      //textフォームの入力値の取得
+      var text = event.target.text.value;
+      //mongoDBへの登録
+      Tasks.insert({
+        text: text,
+        createdAt: new Date() // current time
+      });
+      event.target.text.value = "";
+    }
+  })
 }
